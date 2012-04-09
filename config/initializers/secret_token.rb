cd1872be9d80789e9d0cf31fdaa044fb4f1c4718
John-Paul Taylor <john-paul.taylor@hotmail.co.uk>
2012-04-09 12:23:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRailsProject::Application.config.secret_token = 'fc923544d7c08d0e867f74c2669e219dcae10ad7b5e61b9cc653eb72e3f20f83a3387f4316fb899e697f883952d3f9eacde204afe908fdff6c8f0491700c8c0b'
